da54d8a8509f571a5d50620bd4a0105141d79c91
Rui Zeng <ruizeng.always@outlook.com>
2026-08-28 22:36:07 +1000

    feat: add rbac terraform module

---
+++ terraform/modules/rbac/versions.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_providers {
+    snowflake = {
+      source  = "snowflakedb/snowflake"
+      version = ">= 2.19.0, < 3.0.0"
+
+      configuration_aliases = [
+        snowflake.securityadmin,
+        snowflake.sysadmin,
+      ]
+    }
+  }
+}
