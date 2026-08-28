6a28d0ba3202439b795d5c041c42bdbf52085e33
Rui Zeng <ruizeng.always@outlook.com>
2026-08-29 09:11:15 +1000

    feat: compose UAT workload identity bootstrap

---
+++ terraform/stacks/identity/uat/main.tf
@@ -0,0 +1,13 @@
+locals {
+  config = yamldecode(file("${path.root}/../../../../config/environments/uat.yml"))
+}
+
+module "workload_identity" {
+  source = "../../../modules/workload-identity"
+
+  service_user_name = local.config.terraform_identity.user_name
+  role_name         = local.config.terraform_identity.role_name
+  oidc_issuer       = local.config.terraform_identity.oidc_issuer
+  oidc_subject      = local.config.terraform_identity.oidc_subject
+  oidc_audience     = var.oidc_audience
+}
